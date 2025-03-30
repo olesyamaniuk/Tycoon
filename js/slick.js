1f90366cef176bc03c1219ae453b88e516ff29a5
olesyamaniuk <72089671+olesyamaniuk@users.noreply.github.com>
2025-03-30 18:52:40 +0300

    1

---
+++ js/slick.js
@@ -0,0 +1,49 @@
+$(function () {
+  $(".business-gallery-list").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    speed: 1000,
+    dots: true,
+    centerMode: true,
+    cssEase: "linear",
+    prevArrow: ".business-gallery-list-left",
+    nextArrow: ".business-gallery-list-right",
+  });
+});
+
+$(function () {
+  $(".business-reviews-list").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    speed: 1000,
+    dots: true,
+    centerMode: true,
+    cssEase: "linear",
+    prevArrow: ".business-reviews-list-left",
+    nextArrow: ".business-reviews-list-right",
+  });
+});
+$(function () {
+  $(".business-why-list").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    speed: 1000,
+    dots: true,
+    centerMode: true,
+    cssEase: "linear",
+    prevArrow: ".business-why-list-left",
+    nextArrow: ".business-why-list-right",
+  });
+});
